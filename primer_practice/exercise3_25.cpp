d75ca4d7ab29c6e8d4e6f96d9977663fd996d6da
lulibioe <lu_li_ucb@berkeley.edu>
2024-04-18 15:35:39 -0700

    iterator score segmentation && why iterator cannot be added

--- primer_practice/exercise3_25.cpp
+++ primer_practice/exercise3_25.cpp
@@ -4,6 +4,10 @@
 #include <vector>
 using namespace std;
 
+/*将两个指针相减可以表示两个指针(在同一数组中)相距的距离，
+将指针加上一个整数也可以表示移动这个指针到某一位置。
+但是两个指针相加并没有逻辑上的意义，因此两个指针不能相加。
+*/
 int main() {
     vector<unsigned> res(11, 0);  // 100 iteself is a score segment. 11 seg
     unsigned score;
