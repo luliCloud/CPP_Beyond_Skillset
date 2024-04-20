c95d872e151ba34e10a2aa61d8b6bd0813188820
lulibioe <lu_li_ucb@berkeley.edu>
2024-04-20 12:41:16 -0700

    cin input with space, tab, and new line, also double char. even input as sentence but not single char

--- primer_practice/exercise5_11.cpp
+++ primer_practice/exercise5_11.cpp
@@ -1,4 +1,5 @@
-/* 从标准输入读入字符，包括制表符，空格，换行符。并计算它们的个数*/
+/* 从标准输入读入字符，包括制表符，空格，换行符。并计算它们的个数
+ 同时还可以读入 ff， fl，fi。 注意如果i包括在fi里，既要增加i也要增加fi*/
 #include <iostream>
 using namespace std;
 /*使用 std::noskipws 后，每次读取操作都将考虑空白字符，直到再次使用 std::skipws（默认行为）操纵符来恢复跳过空白的行为。
@@ -6,7 +7,8 @@ using namespace std;
 int main() {
     unsigned aCnt = 0, eCnt =0, iCnt = 0, oCnt = 0, uCnt = 0;
     unsigned spaceCnt = 0, tabCnt = 0, newLineCnt = 0;
-    char ch;
+    unsigned ffCnt = 0, flCnt = 0, fiCnt = 0;
+    char ch, prech = '\0';
     while (cin >> noskipws >> ch) { // not skip pws
         switch(ch)
         {
@@ -18,7 +20,8 @@ int main() {
             case'E':
                 eCnt++;
                 break;
-            case'i':
+            case'i':  // noting here
+                if (prech == 'f') fiCnt++;
             case'I':
                 iCnt++;
                 break;
@@ -35,7 +38,18 @@ int main() {
             case'\n':
                 newLineCnt++;
                 break;
+            case'f':
+                if (prech == 'f') {
+                    ffCnt++;
+                }
+                break;
+            case'l':
+                if (prech == 'f') {
+                    flCnt++;
+                }
+                break;
         }
+        prech = ch;  // noting prech is outside of switch condition
     }
     cout << "Number of vowel a(A): \t" << aCnt << '\n'
 		<< "Number of vowel e(E): \t" << eCnt << '\n'
@@ -44,25 +58,33 @@ int main() {
 		<< "Number of vowel u(U): \t" << uCnt << '\n'
 		<< "Number of space: \t" << spaceCnt << '\n'
 		<< "Number of tab char: \t" << tabCnt << '\n'
-		<< "Number of new line: \t" << newLineCnt << endl;
+		<< "Number of new line: \t" << newLineCnt << '\n'
+		<< "Number of ff: \t" << ffCnt << '\n'
+		<< "Number of fl: \t" << flCnt << '\n'
+		<< "Number of fi: \t" << fiCnt << endl;
 
 	return 0;
 }
 
 /*
 output:
-1
-2
-eyrytyiqwiqer "   
-sdifndnslla; 
+fi
+fl
+ff
+iii
+  
+aeiuofsldlf 
 Number of vowel a(A):   1
-Number of vowel e(E):   2
-Number of vowel i(I):   3
+Number of vowel e(E):   1
+Number of vowel i(I):   5
 Number of vowel o(O):   0
-Number of vowel u(U):   0
-Number of space:        5
-Number of tab char:     1
-Number of new line:     4
+Number of vowel u(U):   1
+Number of space:        3
+Number of tab char:     2
+Number of new line:     6
+Number of ff:   1
+Number of fl:   1
+Number of fi:   1
 */
 
 
