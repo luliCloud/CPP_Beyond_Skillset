08a018209c180e0c83f47c7660ef7018675b0cf4
lulibioe <lu_li_ucb@berkeley.edu>
2024-04-21 21:17:19 -0700

    alias, const iterator and recursion

---
+++ primer_practice/exercise6_33.cpp
@@ -0,0 +1,22 @@
+/** 编写一个递归函数，输出vector对象的内容。使用alias，iterator */
+#include <iostream>
+#include <vector>
+using namespace std;
+using Iter = vector<int>::const_iterator;
+
+void print(Iter first, Iter last) {
+    if (first == last) {
+        cout << "meet the end" << endl;
+        return;
+    }
+    cout << *first << " ";
+    print(++first, last);  // noting using ++first. if using first++, always 1.
+    // never meet the end. Also it is not reference, so we will never change first val
+}
+
+int main() {
+    vector<int> v = {1,2,3,4,5,6,6}; // using initializer_list to initialize vector
+    // noting we use cbegin for const iterator
+    print(v.cbegin(), v.cend());
+    return 0;
+}
