9f48cc8f0baee8041eeb11bb464d8773e6aaaddf
luliCloud <greenpearll0804@gmail.com>
2024-06-12 14:23:04 -0700

    thread local var is not affected each other (they are independent)

--- concurrent_programming/thread_local_storage.cpp
+++ concurrent_programming/thread_local_storage.cpp
@@ -18,10 +18,13 @@ int main() {
     thread t2(increamentThreadLocal);
     t1.join();
     t2.join();
+    cout << "Main thread local value: " << threadLocalValue << endl;
     return 0;
 }
 /**
  * Thread local value in thread 140737345746496 is 1
 
 Thread local value in thread 140737337353792 is 1
+
+Main thread local value: 0  (用vs compile才有这个规整的结果。否则两句话是混乱的顺序)
  */
