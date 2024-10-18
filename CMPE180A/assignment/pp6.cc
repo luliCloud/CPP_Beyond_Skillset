4b3fb42dc6a26b78eacec6d2535f246a44db8e9a
Lu from sjsu <lu.li02@sjsu.edu>
2024-10-18 06:12:01 +0000

    pp6

---
+++ CMPE180A/assignment/pp6.cc
@@ -0,0 +1,32 @@
+#include "pp6.h"
+#include <iostream>
+#include <algorithm>
+
+string mergeString(const vector<string> &a) {
+    int n = a.size();
+    string res = "";
+
+    int max_len = 0; // find the max_len in a
+    for (const string& sub : a) {
+        int sz = sub.size();
+        max_len = max(sz, max_len);
+    }
+
+    for (int i = 0; i < max_len; i++) {
+        // inner loop iterate every string inside a
+        for (int j = 0; j < n; j++) {
+            if (i < a[j].size()) {
+                res += a[j][i];
+            }
+        }
+    }
+    return res;
+}
+
+int main() {
+    vector<string> a = {"abc", "e", "defghji"};
+
+    string res = mergeString(a);
+    cout << res << endl;
+    return 0;
+}
